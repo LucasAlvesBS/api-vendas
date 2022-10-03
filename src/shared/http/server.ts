b4ce40dd920804a15796ac1fc2accd09e7e35c1d
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-03 10:12:45 -0300

    feat: add express-async-errors lib for error handling

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/app-error';
