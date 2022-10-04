6a0cdbf66d42384c756f6794f709900786e693c8
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-04 10:16:53 -0300

    feat: add users controller to communicate with service

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import DeleteUserService from '../services/DeleteUserService';
+import ListUsersService from '../services/ListUsersService';
+import ShowUserService from '../services/ShowUserService';
+import UpdateUserService from '../services/UpdateUserService';
+
+export default class UserController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUsers = new ListUsersService();
+
+    const users = await listUsers.execute();
+
+    return response.status(200).json(users);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showUser = new ShowUserService();
+
+    const user = await showUser.execute({ id });
+
+    return response.status(200).json(user);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.status(201).json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const { id } = request.params;
+
+    const updateProduct = new UpdateUserService();
+
+    await updateProduct.execute({ id, name, email, password });
+
+    return response.status(204).json();
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteProduct = new DeleteUserService();
+
+    await deleteProduct.execute({ id });
+
+    return response.status(204).json();
+  }
+}
