9365cd49cf4e6bbd30e434de65e787668e71c33d
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-04 09:15:17 -0300

    feat: add service of show user based on id

---
+++ src/modules/users/services/ShowUserService.ts
@@ -0,0 +1,25 @@
+import { messageHelper } from '@helpers/message.helper';
+import AppError from '@shared/errors/app-error';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRquest {
+  id: string;
+}
+
+class ShowUserService {
+  public async execute(id: IRquest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findOne(id);
+
+    if (!user) {
+      throw new AppError(messageHelper.NOT_FOUND, 404);
+    }
+
+    return user;
+  }
+}
+
+export default ShowUserService;
