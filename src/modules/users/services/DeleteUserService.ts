16f15e3e59b3a6ce2104ba085aad8280771b3750
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-04 09:28:40 -0300

    feat: add delete user service

---
+++ src/modules/users/services/DeleteUserService.ts
@@ -0,0 +1,24 @@
+import { messageHelper } from '@helpers/message.helper';
+import AppError from '@shared/errors/app-error';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRquest {
+  id: string;
+}
+
+class DeleteUserService {
+  public async execute(id: IRquest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findOne(id);
+
+    if (!user) {
+      throw new AppError(messageHelper.NOT_FOUND, 404);
+    }
+
+    await usersRepository.remove(user);
+  }
+}
+
+export default DeleteUserService;
